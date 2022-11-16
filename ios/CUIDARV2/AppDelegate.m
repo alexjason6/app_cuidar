3376726c62a3e666e6a73e6b0ed053d77949ee6f
Alex Jason <39537192+alexjason6@users.noreply.github.com>
2022-11-16 18:28:04 -0300

    Update AppDelegate.m

--- ios/CUIDARV2/AppDelegate.m
+++ ios/CUIDARV2/AppDelegate.m
@@ -30,7 +30,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  [GMSServices provideAPIKey:@"AIzaSyBxRVrjEmcIGKpxukpcXC2b6Cec_jxcEt8"];
+  [GMSServices provideAPIKey:@""];
   // Add me --- \/
    if ([FIRApp defaultApp] == nil) {
      [FIRApp configure];
